c9cddb97a883e7ebc7a979de433729078323804c
niespihu12 <niespihu12@gmail.com>
2024-05-23 15:43:14 -0500

    first commit

--- config/database.php
+++ config/database.php
@@ -1,10 +1,25 @@
 <?php
     class Database{
-        private $hostname = "localhost";
-        private $database = "nical";
-        private $username = "root";
-        private $password = "admin";
-        private $charset = "utf8";
+        private $hostname;
+        private $database;
+        private $username;
+        private $password;
+        private $charset;
+
+        function __construct(){
+            $this->loadEnv();
+        }
+
+        private function loadEnv(){
+            $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+            $dotenv->load();
+
+            $this->hostname = $_ENV['DB_HOST'];
+            $this->database = $_ENV['DB_DATABASE'];
+            $this->username = $_ENV['DB_USERNAME'];
+            $this->password = $_ENV['DB_PASSWORD'];
+            $this->charset = $_ENV['DB_CHARSET'];
+        }
 
         function conectar(){
             try{
@@ -19,10 +34,8 @@
 
                 return $pdo;
             }catch(PDOException $e){
-                echo 'Error conexion:' .$e->getMessage();
+                echo 'Error conexión:' . $e->getMessage();
                 exit;
-
-
             }
         }
     }
